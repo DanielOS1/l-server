55338a84d4dbc53fe5bf4c1472cc2aac8c348480
DanielOS1 <danielolivares60@gmail.com>
2026-01-25 19:57:44 -0300

    fix: config db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,21 +4,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const getDatabaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => {
-  const password = configService.get<string>('DATABASE_PASSWORD');
-  if (!password) {
-    throw new Error('DATABASE_PASSWORD is not defined');
+  const databaseUrl = configService.get<string>('DATABASE_URL');
+
+  if (!databaseUrl) {
+    throw new Error('DATABASE_URL is not defined');
   }
 
   return {
     type: 'postgres',
-    host: configService.get<string>('DATABASE_HOST'),
-    port: parseInt(configService.get<string>('DATABASE_PORT') || '5432', 10),
-    username: configService.get<string>('DATABASE_USERNAME'),
-    password: password,
-    database: configService.get<string>('DATABASE_NAME'),
-    entities: [__dirname + '/../**/*.entity.{ts,js}'],
-    synchronize: process.env.NODE_ENV !== 'production', // Disable in production!
-    logging: process.env.NODE_ENV !== 'production',
+    url: databaseUrl,
     autoLoadEntities: true,
+    synchronize: process.env.NODE_ENV !== 'production',
+    logging: process.env.NODE_ENV !== 'production',
   };
 };
